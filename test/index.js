d902d8ef23f76e815f770a57ee77d4447203289f
Andri Möll <andri@dot.ee>
2021-10-14 23:23:47 +0000

    Fix typo in comment.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,3 @@
-// Must.js doesn't yet ssupport comparing Maps out of the box.
+// Must.js doesn't yet support comparing Maps out of the box.
 /* eslint no-extend-native: 0 */
 Map.prototype.valueOf = function() { return Array.from(this) }
